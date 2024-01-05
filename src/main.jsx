78041f9bfe85d44c71aca0d13924c9c37e8e7e29
JOÃO PEDRO SILVEIRA PEREIRA DE FIGUEIREDO SANTOS <JOO0520@DIGITALCOLLEGE.LOCAL>
2024-01-04 21:12:18 -0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { NotFound } from './pages/notFound/NotFound.jsx'
+import { Produtos } from './pages/produtos/Produtos.jsx'
+import { Home } from './pages/home/Home.jsx'
+import { Categorias } from './pages/categorias/Categorias.jsx'
+import { Login } from './pages/login/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<App />}>
+          <Route path="" element={<Home/>} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/categorias" element={<Categorias/>} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
